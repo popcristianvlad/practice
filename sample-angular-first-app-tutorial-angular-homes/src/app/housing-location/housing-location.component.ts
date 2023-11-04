887b045bc1752daffb3692f0f609c16618a3c5c5
Vlad Pop <popcristianvlad@gmail.com>
2023-11-04 12:17:05 +0200

    Customized the details page

--- sample-angular-first-app-tutorial-angular-homes/src/app/housing-location/housing-location.component.ts
+++ sample-angular-first-app-tutorial-angular-homes/src/app/housing-location/housing-location.component.ts
@@ -1,20 +1,25 @@
 import {Component, Input} from '@angular/core';
+import {RouterModule} from '@angular/router';
 import {CommonModule} from '@angular/common';
 import {HousingLocation} from "../housinglocation";
 
 @Component({
-  selector: 'app-housing-location',
-  standalone: true,
-  imports: [CommonModule],
-  template: `
-    <section class="listing">
-      <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
-      <h2 class="listing-heading">{{housingLocation.name}}</h2>
-      <h2 class="listing-location">{{housingLocation.city}}, {{housingLocation.state}}</h2>
-    </section>
-  `,
-  styleUrls: ['./housing-location.component.css']
+    selector: 'app-housing-location',
+    standalone: true,
+    imports: [
+        RouterModule,
+        CommonModule
+    ],
+    template: `
+        <section class="listing">
+            <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+            <h2 class="listing-heading">{{housingLocation.name}}</h2>
+            <p class="listing-location">{{housingLocation.city}}, {{housingLocation.state}}</p>
+            <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
+        </section>
+    `,
+    styleUrls: ['./housing-location.component.css']
 })
 export class HousingLocationComponent {
-  @Input() housingLocation!: HousingLocation;
+    @Input() housingLocation!: HousingLocation;
 }
