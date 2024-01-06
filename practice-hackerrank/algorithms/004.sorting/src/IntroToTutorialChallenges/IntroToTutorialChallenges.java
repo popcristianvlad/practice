0ef48f8ae43edc44c0d471254cd427405800012b
Vlad Pop <popcristianvlad@gmail.com>
2024-01-06 13:04:09 +0200

    Remove long packages: HackerRank/Algorithms

---
+++ practice-hackerrank/algorithms/004.sorting/src/IntroToTutorialChallenges/IntroToTutorialChallenges.java
@@ -0,0 +1,44 @@
+package IntroToTutorialChallenges;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+import static java.util.stream.Collectors.toList;
+
+class Result3 {
+
+    public static int introTutorial(int V, List<Integer> arr) {
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) == V) {
+                return i;
+            }
+        }
+
+        // won't be the case
+        return 0;
+    }
+}
+
+public class IntroToTutorialChallenges {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int V = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result3.introTutorial(V, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
---
+++ practice-hackerrank/algorithms/006.graph-theory/src/FloydCityOfBlindingLights/FloydCityOfBlindingLights.java
@@ -0,0 +1,77 @@
+package FloydCityOfBlindingLights;
+
+public class FloydCityOfBlindingLights {
+
+    static int[][] generateFloydMatrix(int numberOfNodes, int numberOfEdges, int[] roadFrom, int[] roadTo, int[] roadWeight) {
+        int[][] floydMatrix = new int[numberOfNodes][numberOfNodes];
+
+        for (int i = 0; i < numberOfNodes; i++) {
+            for (int j = 0; j < numberOfNodes; j++) {
+                if (i == j) {
+                    floydMatrix[i][j] = 0;
+                } else {
+                    floydMatrix[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+
+        for (int i = 0; i < numberOfEdges; i++) {
+            if (roadFrom[i] != roadTo[i]) {
+                floydMatrix[roadFrom[i] - 1][roadTo[i] - 1] = roadWeight[i];
+            }
+        }
+
+        for (int k = 0; k < numberOfNodes; k++) {
+            for (int i = 0; i < numberOfNodes; i++) {
+                for (int j = 0; j < numberOfNodes; j++) {
+                    int sum = sum(floydMatrix[i][k], floydMatrix[k][j]);
+
+                    if (sum < floydMatrix[i][j]) {
+                        floydMatrix[i][j] = sum;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < numberOfNodes; i++) {
+            for (int j = 0; j < numberOfNodes; j++) {
+                if (floydMatrix[i][j] == Integer.MAX_VALUE) {
+                    floydMatrix[i][j] = -1;
+                }
+            }
+        }
+
+        return floydMatrix;
+    }
+
+    static int sum(int a, int b) {
+        if (a == Integer.MAX_VALUE || b == Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+
+        return a + b;
+    }
+
+    static int getWeightForNodes(int[][] floydMatrix, int x, int y) {
+        return floydMatrix[x - 1][y - 1];
+    }
+
+    public static void main(String[] args) {
+        main_hardcodedValues();
+    }
+
+    private static void main_hardcodedValues() {
+        int numberOfNodes = 4;
+        int numberOfEdges = 5;
+
+        int[] roadFrom = new int[]{1, 1, 2, 3, 3};
+        int[] roadTo = new int[]{2, 4, 4, 4, 2};
+        int[] roadWeight = new int[]{5, 24, 6, 4, 7};
+
+        int[][] floydMatrix = generateFloydMatrix(numberOfNodes, numberOfEdges, roadFrom, roadTo, roadWeight);
+
+        System.out.println(getWeightForNodes(floydMatrix, 1, 2));
+        System.out.println(getWeightForNodes(floydMatrix, 3, 1));
+        System.out.println(getWeightForNodes(floydMatrix, 1, 4));
+    }
+}
